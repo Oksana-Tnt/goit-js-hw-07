6a2f90630099dc6a8a66d2a91b634cb770dc3ab5
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-07-10 21:02:18 +0200

    task-01

---
+++ js/01-gallery.js
@@ -0,0 +1,70 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector(".gallery");
+   
+    const markup = galleryItems.map(({ preview, original, description}) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+
+);
+
+galleryEl.insertAdjacentHTML("beforeend", markup.join(""));
+
+galleryEl.addEventListener("click", onClick);
+document.addEventListener("keydown", closeModalView);
+let instance;
+
+function onClick(event){
+
+    event.preventDefault();
+
+    if(!event.target.classList.contains("gallery__image")){
+        return;
+    }
+
+    const sourceImg = event.target.dataset.source;
+    const currentElement = galleryItems.find(({original}) => original === sourceImg);
+
+    instance = basicLightbox.create(`
+    <div class="modal">
+    <img
+        class="gallery__image"
+        src="${currentElement.original}"        
+        alt="${currentElement.description}"
+      />
+    </div>
+`)
+
+instance.show(); 
+};
+
+function closeModalView(event){
+    if(instance.visible() && event.code === "Escape"){
+        instance.close();
+    }
+
+};
+
+
+
+
+
+
+
+
+/*const instance = basicLightbox.create(`
+	<h1>Dynamic Content</h1>
+	<p>You can set the content of the lightbox with JS.</p>
+`)
+console.log(instance);
+instance.show();*/
+
+
