fb54a8efbcb8a05718aa56b41b2e906f7f3c4e5c
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-07-10 22:54:27 +0200

    task-02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -20,6 +20,7 @@ galleryEl.insertAdjacentHTML("beforeend", markup.join(""));
 
 galleryEl.addEventListener("click", onClick);
 document.addEventListener("keydown", closeModalView);
+
 let instance;
 
 function onClick(event){
@@ -59,12 +60,5 @@ function closeModalView(event){
 
 
 
-
-/*const instance = basicLightbox.create(`
-	<h1>Dynamic Content</h1>
-	<p>You can set the content of the lightbox with JS.</p>
-`)
-console.log(instance);
-instance.show();*/
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+   
+    const markup = galleryItems.map(({ preview, original, description}) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+
+);
+
+galleryEl.insertAdjacentHTML("beforeend", markup.join(""));
+
+galleryEl.addEventListener("click", onClick);
+
+
+
+
+function onClick(event){
+
+    event.preventDefault();
+
+    if(!event.target.classList.contains("gallery__image")){
+        return;
+    }
+
+    let lighbox = new SimpleLightbox('.gallery a', { captionsData: `"${event.target.alt}"`,captionDelay: 250, captionPosition: "bottom"});
+
+    lighbox.on("show.simplelightbox");
+  
+    
+
+
+};
+
+
+
 
 console.log(galleryItems);
